9a315147c22833efcec03a4359e65d124cf253c6
Ashish Agrahari <44419439+AshishA26@users.noreply.github.com>
2026-05-24 10:54:02 -0400

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,53 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # -------------------- SOLUTION 1 ---------------------
+        # # Use a hashmap to hold the number of times a letter occurs
+        # # and see if that matches the hashmap for the 2nd string
+        # # Time complexity: O(s + t) or basically O(n)
+
+        # # Edge case. Ensure same length
+        # if (len(s) != len(t)): 
+        #     return False
+
+        # sMap = {} # char : count
+        # tMap = {} # char : count
+
+        # for c in s:
+        #     if c in sMap.keys():
+        #         sMap[c] += 1
+        #     else:
+        #         sMap[c] = 0
+        
+        # for c in t:
+        #     if c in tMap.keys():
+        #         tMap[c] += 1
+        #     else:
+        #         tMap[c] = 0
+
+        # for key in sMap.keys():
+        #     if key in tMap.keys() and sMap[key] == tMap[key]:
+        #         continue # Matches
+        #     if key not in tMap.keys() or sMap[key] != tMap[key]:
+        #         return False
+
+        # return True
+
+        # -------------------- SOLUTION 2 ---------------------
+        if (len(s) != len(t)): 
+            return False
+        sMap = {} # char : count
+        tMap = {} # char : count
+        # Rather than the 2 for loops above, can iterate at the same time
+        # because the lists are ensured to be the same length.
+        # Use .get to avoid needing if statements I did above.
+        for i in range(len(s)):
+            sMap[s[i]] = 1 + sMap.get(s[i], 0)
+            tMap[t[i]] = 1 + tMap.get(t[i], 0)
+        # Check that counts of each character matches
+        for c in sMap:
+            if sMap[c] != tMap.get(c, 0): # Use .get incase character doesnt exist in tMap
+                return False
+        return True
+
+        # -------------------- SOLUTION 3 ---------------------
+        # return Counter(s) == Counter(t)
