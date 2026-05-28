f3abf7c3b7652b09e641d50264f8d134989f1c71
Ashish Agrahari <44419439+AshishA26@users.noreply.github.com>
2026-05-28 00:22:42 -0400

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # Make a hashmap with the unique anagrams. Unique meaning that we sort the strings first,
+        # then check if a string exists in the hashmap. The hashmap has a key of the sorted string,
+        # with a value of a list of anagrams.
+        # Or can use Counter(), or other functions
+        
+        anagrams = defaultdict(list) # string : list of strings
+        
+        for s in strs:
+            sorted_s = "".join(sorted(s)) # sorted is a list a characters, need to join to make a string
+            anagrams[sorted_s].append(s)
+
+        return list(anagrams.values()) # Need to cast values to a list
