a8bb6c0adf2333b657bf59460b2ec5ca4c070329
Ashish Agrahari <44419439+AshishA26@users.noreply.github.com>
2026-09-15 02:05:25 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # ************* SOLUTION 1 **************
+        # Remove spaces and special characters, lowercase all, check if forward = reverse
+        
+        s = s.lower()
+        s = s.replace(" ", "") # Remove spaces. Don't use .strip() as it only does outer edges
+
+        # Only keep alphanumeric chars
+        newS = "".join([ch for ch in s if ch.isalnum()])
+
+        # Compare string to reversed string
+        rev = newS[::-1]
+        if rev == newS:
+            return True
+        return False
